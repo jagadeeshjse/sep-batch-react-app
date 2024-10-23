4ca33710d75b74f8c975f5db10c921e7bd36f749
unknown <jagadeeshganta7@gmail.com>
2024-10-23 07:06:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>heading data</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, nam
+        necessitatibus hic earum ratione esse velit, molestias quibusdam eveniet
+        corrupti minima, ex sit accusantium. Labore pariatur, ratione ad animi
+        iste consectetur explicabo laudantium cum dolorem debitis recusandae hic
+        assumenda! Consequatur?
+      </p>
     </div>
   );
-}
+};
 
 export default App;
